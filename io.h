034039437529a248903381dfcdf32b26a2c34bcd
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2020-01-09 17:55:40 +0100

    Updated io to write on file

--- io.h
+++ io.h
@@ -17,19 +17,6 @@ Problem* getProblemFromFile(std::string instance_name, int max_time);
 
 /*** OUTPUT ***/
 
-/*  Check if a given file exists, so we can read it  */
-bool isFileExist(std::string fileName);
-
-/*  Read the file with the best solution penalty accomplished  */
-double readBestSolutionPenalty(std::string instance_name);
-
-/*  Write the solution to a file, i.e. the timeslots assigned for each exam  */
-void writeBestSolution(int *exams_timeslot, int n_exams, std::string instance_name_best_solution);
-
-/*  Write the best solution penalty to a file, so we can compare with future results  */
-void writeBestSolutionPenalty(double penalty, std::string instance_name_best_penalty);
-
-/*  Main output function  */
 void writeSolutionOnFile(Problem *p);
 
 #endif //IO_CPP
