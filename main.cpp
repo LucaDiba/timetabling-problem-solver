4e5962e571d272331fe4a048029e58c3fc37ce69
Luca Dibattista <34377738+LucaDiba@users.noreply.github.com>
2020-01-12 22:51:37 +0100

    Adjusted parameters

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-#include <utility>
 
 #include <random>
+#include <functional>
+
 std::mt19937 generator;
 
 #include "data-structures/Problem.h"
@@ -13,16 +14,19 @@ std::mt19937 generator;
 int main(int argc, char * argv[]) {
 
     std::string instance_name = argv[1];
-    int max_time = atoi(argv[2]);
+    int max_time = atoi(argv[3]);
+    printf("max time: %d\n", max_time);
 
 
-    int max_multistart_time = int(max_time * 0.6);
-    int max_neighborhood_time = max_time - max_multistart_time;
+    int max_multistart_time = int(max_time * 0.8);
 
     Problem *problem = getProblemFromFile(instance_name, max_time);
 
     printf("Multistart\n");
     multistart(problem, max_multistart_time);
+
+    int elapsed_time = (time(nullptr) - problem->start_time);
+    int max_neighborhood_time = problem->max_time - elapsed_time;
 
     printf("Neighborhood\n");
     neighborhood(problem, max_neighborhood_time);
